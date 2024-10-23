7012ef0f6646bff081ec4db4bcfe16e561bea836
pdx01 <pradeep.saini@emeritus.org>
2024-10-23 23:38:39 +0530

    UI updated

--- src/main/resources/static/js/searchtext.js
+++ src/main/resources/static/js/searchtext.js
@@ -26,9 +26,18 @@
             }
         };
  
-    function findAndReplace(courseIds, textToFind, replaceWith) {
+    function findAndReplace() {
+		var confirmationModal = bootstrap.Modal.getInstance(document.getElementById('confirmationModal'));
+        confirmationModal.hide();
+		// Show loader
+        document.getElementById('loader').style.display = 'block';
+        $(".invalid-feedback").hide();
         // Show the horizontal loader before making the request
-        document.getElementById('horizontal-loader').style.display = 'block';
+        //document.getElementById('horizontal-loader').style.display = 'block';
+        
+        var courseIds = document.getElementById('course_id').value;
+        var textToFind = document.getElementById('targetText').value;
+        var replaceWith = document.getElementById('updatedText').value;
 
         // Convert the comma-separated string into an array (trim to remove extra spaces)
         let courseIdList = courseIds.split(',').map(id => id.trim());
@@ -44,7 +53,7 @@
             }),
             success: function(data) {
                 // Hide the loader when request is successful
-                document.getElementById('horizontal-loader').style.display = 'none';
+                //document.getElementById('horizontal-loader').style.display = 'none';
                 console.log('Text replaced successfully!');
                 populateResult(courseIds, textToFind, replaceWith);
             },
@@ -57,8 +66,73 @@
     }
 
 // Function to populate result or redirect to another page with parameters
-function populateResult(courseIds, textToFind, replaceWith) {
-  window.location.href = appContext + 'ui/v1/search-result?courseIds=' + courseIds +
-    '&textToFind=' + textToFind + '&textToReplace=' + replaceWith;
+function populateResult(courseIds, textToFind, replaceWith) {     
+     $.ajax({
+            type: "GET",
+            url: appContext + 'ui/v1/search-result?courseIds=' + courseIds +
+    '&textToFind=' + textToFind + '&textToReplace=' + replaceWith,
+            contentType: "application/json",
+            success: function(data) {
+                $("#searchResult").html(data);
+                // hide loader
+       			 document.getElementById('loader').style.display = 'none';
+       			 $("#resetButton").removeClass("d-none");
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                // Hide the loader when an error occurs
+                document.getElementById('horizontal-loader').style.display = 'none';
+                // hide loader
+       			document.getElementById('loader').style.display = 'none';
+                console.log("Error while replacing text: " + errorThrown);
+            }
+        });
+}
+
+$(document).on('click', '.accordion-button', function(e) {
+	var target = $(this).attr('data-bs-target');
+	console.log($("#"+target).html());
+	if($("#"+target).hasClass('show')) {
+		$("#"+target).removeClass('show');
+		$(this).addClass("collapsed");
+		$(this).attr("aria-expanded","false");
+	} else {
+		$("#"+target).addClass('show');
+		$(this).removeClass("collapsed");
+		$(this).attr("aria-expanded","true");
+	}
+	
+});
+
+// Reset form and hide loader on reset
+$(document).on('click', '#resetButton', function(e) {
+	document.getElementById('loader').style.display = 'none';
+	document.getElementById('searchResult').innerHTML = '';
+	$("#resetButton").addClass("d-none");
+	$(".invalid-feedback").hide();
+});
+
+$(document).on('click', '#submitButton', function(e) {
+		$(".invalid-feedback").hide();
+		var courseIds = document.getElementById('course_id').value;
+		if(isNullOrBlank(courseIds)) {
+			$("#invalid-course_id").show();
+			return false;
+		}
+        var targetText = document.getElementById('targetText').value;
+      	if(isNullOrBlank(targetText)) {
+			$("#invalid-targetText").show();
+			return false;
+		}
+        var updatedText = document.getElementById('updatedText').value;
+        
+        var confirmationModal = new bootstrap.Modal(document.getElementById('confirmationModal'));
+        confirmationModal.show();
+});
+
+// check if string null or blank or undefined
+function isNullOrBlank(string){
+    if(string == '' || string == null || string == undefined)
+        return true;
+    return false;
 }
 
